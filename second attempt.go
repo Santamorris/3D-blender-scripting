d5fc892f4b1e19cd46bed19fda91c5e6a9308686
Santa Morris <152790004+Santamorris@users.noreply.github.com>
2023-12-03 15:42:50 +0200

    Update second attempt.go

--- second attempt.go
+++ second attempt.go
@@ -3,3 +3,10 @@ validate-number(){
   if [[ ! $1 =~ $re ]]; then
     echo "Error: The value must be a number." >&2
     exit 1
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
+)
