a54f9418b56e699408204eea68d810659d6ea6af
Santa Morris <152790004+Santamorris@users.noreply.github.com>
2023-12-22 17:51:13 +0200

    Update second attempt.go

--- second attempt.go
+++ second attempt.go
@@ -10,3 +10,17 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
 )
+
+	func (suite *IntegrationTestSuite) TestAppleIAPOrderExist() {
+	k := suite.k
+	ctx := suite.ctx
+	require := suite.Require()
+
+	items := createNAppleIAPOrder(k, ctx, 10)
+	for _, item := range items {
+		require.True(k.HasAppleIAPOrder(ctx, item.PurchaseId))
+			items := createNAppleIAPOrder(k, ctx, 10)
+	require.Equal(items, k.GetAllAppleIAPOrder(ctx))
+	require.Equal(items, k.GetAllAppleIAPOrder(ctx))
+	}
+}
